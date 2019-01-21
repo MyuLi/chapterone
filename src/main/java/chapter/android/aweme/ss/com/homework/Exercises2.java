c4f2c6902992f7e22f85b57417c507c5f8111226
yuyuyuolalalala <45203318+yuyuyuolalalala@users.noreply.github.com>
2019-01-21 11:41:02 +0800

    exercise 2

--- src/main/java/chapter/android/aweme/ss/com/homework/Exercises2.java
+++ src/main/java/chapter/android/aweme/ss/com/homework/Exercises2.java
@@ -3,7 +3,10 @@ package chapter.android.aweme.ss.com.homework;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * 作业2：一个抖音笔试题：统计页面所有view的个数
@@ -16,10 +19,30 @@ public class Exercises2 extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatroom);
+        getAllChildViewCount();
     }
+    private int count = 0;
+    public int getAllChildViewCount() {
+        ViewGroup viewGroup = getWindow().getDecorView().findViewById(android.R.id.content);
+        ViewGroup rootView = (ViewGroup)viewGroup.getChildAt(0);
+        traversalView(rootView);
 
-    public int getAllChildViewCount(View view) {
-        //todo 补全你的代码
+        TextView tv_content_info;
+        tv_content_info = findViewById(R.id.tv_content_info);
+
+        tv_content_info.setText("共有"+count+"view"+"\n");
+        Log.d("count",count+"");
         return 0;
+    }
+    private void traversalView(ViewGroup rootview) {
+        for (int i = 0; i < rootview.getChildCount(); i++){
+            View child_view = rootview.getChildAt(i);
+            if(child_view instanceof ViewGroup){
+                traversalView((ViewGroup) child_view);
+            }
+            else count++;
+        }
+        count++;
     }
 }
